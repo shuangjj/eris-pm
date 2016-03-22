6f5c392aab9826b5e0bb5b650f6ca42c9f656a69
VoR0220 <catalanor0220@gmail.com>
2016-03-22 15:17:39 -0500

    fleshed out readme, solidity contract, and the test...failing...investigating

--- tests/fixtures/app30/app30.sol
+++ tests/fixtures/app30/app30.sol
@@ -1,16 +1,31 @@
- contract multiReturn {
+contract multiReturn {
 
-	struct Strings {
-		string filename;
-		string username;
-	}
-
-	function getStatus() returns (uint, uint) {
+	function getInts() returns (uint, uint) {
   		return (1, 2);
 	}
 	function getStrings() returns (string filename, string username) {
-	    
-		return (filename, username);
+		filename = "PlansForWorldDomination.md";
+		username = "DougTheMarmot";
+	}
+	function getBools() returns (bool, bool) {
+		return (true, false);
+	}
+	function getBytes() returns (bytes32, bytes32) {
+		return ("Hello", "World");
+	}
+	function getInterMixed() 
+		returns (
+			string filename,
+			bool isPresent, 
+			uint copies, 
+			string username, 
+			bytes crypto
+		) {
+		filename = "PranksAndStuff.js";
+		copies = 3;
+		isPresent = true;
+		username = "DougTheMarmot";
+		crypto = "0x36PrZ1KHYMpqSyAQXSG8VwbUiq2EogxLo2";
 	}
 }
 
---
+++ tests/fixtures/app30/readme.md
@@ -0,0 +1,1 @@
+* tests multiple value returns of both static and dynamic values for fully intact tuple
